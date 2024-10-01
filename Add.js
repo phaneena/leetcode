c7433d9c9a3d0887b5f036a668f82f841bf1c2c2
Haneena P <85494776+phaneena@users.noreply.github.com>
2024-10-01 09:44:09 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,7 @@
+num1=12
+num2=5
+function sum(num1,num2){
+    let result=num1+num2
+    return result
+}
+console.log(sum(num1,num2))
---
+++ multiple.js
@@ -0,0 +1,6 @@
+function mult(n){
+    if(n%2==0)
+        return n
+    return n*2
+}
+console.log(mult(5))
