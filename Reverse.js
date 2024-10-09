e1c42ec4bdb9a2886afa66352c4186d27c2a73a6
Haneena P <85494776+phaneena@users.noreply.github.com>
2024-10-09 10:50:04 +0530

    Add files via upload

---
+++ Reverse.js
@@ -0,0 +1,14 @@
+s = ["h","e","l","l","o"]
+function reverse(s){
+    // let result=[]
+    // let k=0
+    // for(i=s.length-1;i>=0;i--){
+    //     result[k]=s[i]
+    //     k++;
+        
+    // }
+    // return result
+    return s.reverse()
+    
+}
+console.log(reverse(s))
