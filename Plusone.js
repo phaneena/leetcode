6fa0899ca757a9c4c4f1e3b4e8936f22e307af81
Haneena P <85494776+phaneena@users.noreply.github.com>
2024-10-14 20:14:10 +0530

    Add files via upload

---
+++ Plusone.js
@@ -0,0 +1,14 @@
+digits = [1,2,3]
+function plusone(digits){
+    for(i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++
+            return digits
+        }
+        digits[i]=0
+    }
+    digits.unshift(1)
+    return digits
+
+}
+console.log(plusone(digits))
