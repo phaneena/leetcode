802d8f3f72588b64579abed0f9136e4564452613
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-01-14 10:16:51 +0530

    Time: 1 ms (44.85%), Space: 49.1 MB (27.61%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.js
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let count=0
-    for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
+    let goodPairs=0
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
             if(nums[i]===nums[j]){
-                count++
+                goodPairs++
             }
         }
     }
-    return count
+    return goodPairs
 };
