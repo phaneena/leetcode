29503e9bb8dbe037c73a79279beb4192d852c91e
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-02-15 15:55:06 +0530

    Time: 1 ms (41.54%), Space: 52.7 MB (44.84%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
@@ -0,0 +1,9 @@
+function runningSum(nums: number[]): number[] {
+    let result=[]
+    let sum=0
+    for(let i=0;i<nums.length;i++){
+        sum=sum+nums[i]
+        result.push(sum)
+    }
+    return result
+};
