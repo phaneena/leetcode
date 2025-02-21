8a66f2415f05a559bce900230f0a98ad29f04c2e
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-02-21 09:41:00 +0530

    Time: 5 ms (37.5%), Space: 57.9 MB (5.36%) - LeetHub

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.ts
@@ -0,0 +1,12 @@
+function countKDifference(nums: number[], k: number): number {
+    let count=0
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]-nums[j]==k){
+                count++
+            }
+        }
+        
+    }
+    return count
+};
