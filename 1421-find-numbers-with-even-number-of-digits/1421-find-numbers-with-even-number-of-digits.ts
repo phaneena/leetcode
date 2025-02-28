f9a3fbe262b18c7d63bad9bac2b7e19dc15e76ec
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-02-28 09:58:30 +0530

    Time: 2 ms (60.06%), Space: 57.2 MB (10.22%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.ts
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.ts
@@ -1,11 +1,7 @@
 function findNumbers(nums: number[]): number {
-    let n=[]
     let count=0
     for(let i=0;i<nums.length;i++){
-        n.push(nums[i].toString())
-    }
-    for(let i=0;i<n.length;i++){
-        if((n[i].length)%2==0){
+        if((nums[i].toString().length)%2==0){
             count++
         }
     }
