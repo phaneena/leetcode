c575d77d7e8818e16493d859ccab264d56457392
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-03-14 09:31:32 +0530

    Add files via upload

---
+++ maxscore.js
@@ -0,0 +1,33 @@
+s = "011101"
+function max(s){
+    let one=0
+    let zero=0
+    let score=0
+    let left=''
+    let right=''
+    let total=[]
+    for(let i=0;i<s.length-1;i++){
+        left+=s[i]
+        for(let j=i+1;j<s.length;j++){
+            right+=s[j]
+        }
+        for(let k=0;k<left.length;k++){
+            if(left[k]=="0"){
+                zero++
+            }
+        }
+        for(let k=0;k<right.length;k++){
+            if(right[k]=="1"){
+                one++
+            }
+        }
+        score=one+zero
+        total.push(score)
+        right=''
+        one=0
+        zero=0
+    }
+    let maxScore=Math.max(...total)
+    return maxScore
+}
+console.log(max(s))
