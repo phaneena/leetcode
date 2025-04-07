a2663800a545e01a69d47d66bd3b60c3e2250624
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-04-07 09:55:52 +0530

    Time: 10 ms (71.98%), Space: 72.9 MB (57.92%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+     let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        let index = Math.abs(nums[i]) - 1
+        if (nums[index] > 0) nums[index] = -nums[index];
+    }
+
+    
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) result.push(i + 1); 
+    }
+
+    return result;
+};
