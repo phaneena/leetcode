d022e0b027efeba629ed8f71cb176896c80817d5
Haneena P <85494776+phaneena@users.noreply.github.com>
2025-04-28 09:54:08 +0530

    Time: 4 ms (47.74%), Space: 57.8 MB (18.09%) - LeetHub

--- 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.js
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.js
@@ -3,16 +3,29 @@
  * @return {number}
  */
 var countEven = function(num) {
-    let count=0
-    for(i=1;i<=num;i++){
-        let arr=i.toString()
-        let sum=0
-        for(let x of arr){
-            sum+=Number(x)
-        }
-        if(sum%2==0){
-            count++
-        }
+    // let count=0
+    // for(i=1;i<=num;i++){
+    //     let arr=i.toString()
+    //     let sum=0
+    //     for(let x of arr){
+    //         sum+=Number(x)
+    //     }
+    //     if(sum%2==0){
+    //         count++
+    //     }
+    // }
+    // return count
+    let count = 0;
+
+  for (let i = 1; i <= num; i++) {
+    const digitSum = i.toString()
+                      .split('')              
+                      .reduce((sum, digit) => sum + Number(digit), 0);
+    
+    if (digitSum % 2 === 0) {
+      count++;
     }
-    return count
+  }
+
+  return count;
 };
